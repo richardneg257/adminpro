3437ac3c153b1b14ab003bbdc013ef6216237db4
Richard Negron <richard_257@hotmail.com>
2020-09-21 18:39:02 -0500

    header, sidebar, breadcrumbs, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 
@@ -17,7 +17,7 @@ import { HeaderComponent } from './shared/header/header.component';
     RegisterComponent,
     NopagefoundComponent,
     DashboardComponent,
-    BreadcrumsComponent,
+    BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent
   ],
