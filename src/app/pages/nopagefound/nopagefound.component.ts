63d60d94fd345894831557f496bbdfcce369152d
Richard Negron <richard.negron.257@gmail.com>
2020-09-21 23:34:40 -0500

    login register nopagefound

--- src/app/pages/nopagefound/nopagefound.component.ts
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
-  styles: [
-  ]
+  styleUrls: ['./nopagefound.component.css']
 })
 export class NopagefoundComponent implements OnInit {
+
+  year = new Date().getFullYear();
 
   constructor() { }
 
