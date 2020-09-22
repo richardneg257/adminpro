294392f81b97ff942b14be1f0d451496e016d645
Richard Negron <richard.negron.257@gmail.com>
2020-09-21 22:53:35 -0500

    pages.component y rutas secundarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { AppRoutingModule } from './app-routing.module';
     SidebarComponent,
     HeaderComponent,
     ProgressComponent,
-    Grafica1Component
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
