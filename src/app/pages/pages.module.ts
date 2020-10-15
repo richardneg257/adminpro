e4e4a1f7185a4f0a4a65418e14bad2afa585c864
Richard Negron <richard_257@hotmail.com>
2020-10-15 01:24:45 -0500

    componente progress bar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -22,6 +23,7 @@ import { PagesComponent } from './pages.component';
     PagesComponent
   ],
   imports: [
+    FormsModule,
     CommonModule,
     RouterModule,
     SharedModule
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,14 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
+  styleUrls: [
+    './progress.component.css'
   ]
 })
 export class ProgressComponent implements OnInit {
 
+  progreso: number = 25;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get getPorcentaje() {
+    return `${this.progreso}%`;
+  }
+
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor >= 0) {
+      return this.progreso = 100;
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      return this.progreso = 0;
+    }
+    this.progreso = this.progreso + valor;
   }
 
 }
